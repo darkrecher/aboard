d806b54951aae73d5599852bd4efb0cd58e539f2
Recher <recher@recher.com>
2018-07-29 02:49:13 +0200

    Param par défaut pour renderer. Renderer par défaut pour Board

--- code/aboard.py
+++ code/aboard.py
@@ -19,52 +19,49 @@ class Tile():
 		return '.'
 
 
-class Board():
-
-	# TODO : on devrait pouvoir spécifier juste une classe héritée de Tile. Sans lambda.
-	def __init__(self, w=1, h=1, tile_generator=lambda x, y: Tile(x, y)):
-
-		self.w = w
-		self.h = h
-		self._tiles = [
-			[ tile_generator(x, y) for x in range(w) ]
-			for y in range(h)
-		]
-		self._default_renderer = BoardRenderer()
-
-
-	# TODO : faut pos, tuple et x, y
-	def get_tile(self, x, y):
-		return self._tiles[y][x]
-
-
-	def _render_tile(self, tile):
-		pass
-
-	def render(self):
-		# TODO : permettre un autre renderer que le default.
-		return self._default_renderer.render(self)
-
-
 class BoardRenderer():
 
-	def __init__(
-		self,
-		tilepadding_w=0, tilepadding_h=0,
-		tile_w=1, tile_h=1,
-		border_vertic=False, border_horiz=True,
-		chr_fill_padding=' ',
-		chr_fill_tile=' ', word_wrap=False
-	):
-		self.tilepadding_w = tilepadding_w
-		self.tilepadding_h = tilepadding_h
-		self.tile_w = tile_w
-		self.tile_h = tile_h
-		self.border_vertic = border_vertic
-		self.border_horiz = border_horiz
-		self.chr_fill_padding = chr_fill_padding
-		self.chr_fill_tile = chr_fill_tile
-		self.word_wrap = word_wrap
+	AUTHORIZED_ATTRIBUTES = {
+		'tile_w': 1, 'tile_h': 1, 'chr_fill_tile': ' ',
+		'word_wrap': False,
+		'tile_padding_w': 0, 'tile_padding_h': 0,
+		'chr_fill_tile_padding': ' ',
+		'tile_border_vertic': False, 'tile_border_horiz': False,
+		'chr_tile_border_vertic': '|', 'chr_tile_border_horiz': '-',
+		'chr_tile_border_cross': '+',
+		'deduce_board_style_from_tile_style': False,
+		'board_padding_w': 0, 'board_padding_h': 0,
+		'chr_fill_board_padding': ' ',
+		'board_border_vertic': False, 'board_border_horiz': False,
+		'chr_board_border_vertic': '|', 'chr_board_border_horiz': '-',
+		'chr_board_border_cross': '+',
+	}
+
+	NOT_IMPLEMENTED = [
+		'word_wrap',
+		'tile_border_vertic', 'tile_border_horiz',
+		'chr_tile_border_vertic', 'chr_tile_border_horiz',
+		'chr_tile_border_cross',
+		'deduce_board_style_from_tile_style',
+		'board_padding_w', 'board_padding_h',
+		'chr_fill_board_padding',
+		'board_border_vertic', 'board_border_horiz',
+		'chr_board_border_vertic', 'chr_board_border_horiz',
+		'chr_board_border_cross',
+	]
+
+	def __init__(self, **kwargs):
+
+		for key_attr in kwargs:
+			if key_attr not in BoardRenderer.AUTHORIZED_ATTRIBUTES:
+				raise ValueError("Paramètre inconnu : " + key_attr)
+			if key_attr in BoardRenderer.NOT_IMPLEMENTED:
+				raise ValueError("TODO : " + key_attr)
+
+		dict_attrs = dict(BoardRenderer.AUTHORIZED_ATTRIBUTES)
+		dict_attrs.update(kwargs)
+		for key_attr, val_attr in dict_attrs.items():
+			setattr(self, key_attr, val_attr)
 
 
 	def _str_resized(self, value):
@@ -76,9 +73,6 @@ class BoardRenderer():
 		"""
 		Renvoie une liste de tile_h string, chacune ayant une taille tile_w.
 		"""
-		if self.word_wrap:
-			raise NotImplemented("TODO")
-
 		tile_res = tile.render(self.tile_h, self.tile_w)
 
 		if isinstance(tile_res, str):
@@ -106,9 +100,9 @@ class BoardRenderer():
 	def render_iter_lines(self, board):
 		# TODO : utiliser un itérateur de Board
 		render_result = ''
-		interval_tile_w = self.chr_fill_padding * self.tilepadding_w
+		interval_tile_w = self.chr_fill_tile_padding * self.tile_padding_w
 		interval_line_h = interval_tile_w.join((
-			[ self.chr_fill_padding*self.tile_w ] * board.w
+			[ self.chr_fill_tile_padding*self.tile_w ] * board.w
 		))
 
 		for y in range(board.h):
@@ -126,7 +120,7 @@ class BoardRenderer():
 				))
 
 			if y < board.h-1:
-				for index_interval in range(self.tilepadding_h):
+				for index_interval in range(self.tile_padding_h):
 					yield interval_line_h
 
 
@@ -134,8 +128,44 @@ class BoardRenderer():
 		return '\n'.join(self.render_iter_lines(board))
 
 
+class Board():
+
+	# TODO : on devrait pouvoir spécifier juste une classe héritée de Tile. Sans lambda.
+	def __init__(
+		self,
+		w=1, h=1,
+		tile_generator=lambda x, y: Tile(x, y),
+		default_renderer=BoardRenderer(),
+	):
+
+		self.w = w
+		self.h = h
+		self._default_renderer = default_renderer
+
+		self._tiles = [
+			[ tile_generator(x, y) for x in range(w) ]
+			for y in range(h)
+		]
+		self._default_renderer = BoardRenderer()
+
+
+	# TODO : faut pos, tuple et x, y
+	def get_tile(self, x, y):
+		return self._tiles[y][x]
+
+
+	def _render_tile(self, tile):
+		pass
+
+	def render(self, renderer=None):
+		if renderer is None:
+			renderer = self._default_renderer
+		return renderer.render(self)
+
 # tests
 
+board = Board(8, 2)
+log(board.render())
 
 class MyTile(Tile):
 
@@ -155,26 +185,31 @@ log(my_tile.render())
 class MyTileTellCoords(Tile):
 
 	def render(self, w=1, h=1):
-		return [
-			'',
-			str(self.x) + ',' + str(self.y),
-			self.x * self.y
-		]
+		if (w, h) == (1, 1):
+			return hex(self.x * self.y)[2:].upper()
+		else:
+			return [
+				'',
+				' ' + str(self.x) + ',' + str(self.y),
+				self.x * self.y
+			]
 
 
 board = Board(7, 4, lambda x, y: MyTileTellCoords(x, y))
 tile = board.get_tile(0, 0)
 log(tile.render())
 
-board_renderer = BoardRenderer(
+my_board_renderer = BoardRenderer(
 	tile_w=5, tile_h=4,
-	tilepadding_w=3, tilepadding_h=2,
-	chr_fill_tile='.', chr_fill_padding='#',
+	tile_padding_w=3, tile_padding_h=2,
+	chr_fill_tile='.', chr_fill_tile_padding='#',
 )
 
-log(board_renderer._render_tile(tile))
+log(my_board_renderer._render_tile(tile))
 log('')
-log(board_renderer.render(board))
+log(board.render())
+log('')
+log(board.render(my_board_renderer))
 log('')
 
 log("End")
